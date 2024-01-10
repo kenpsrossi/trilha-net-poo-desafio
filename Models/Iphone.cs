86188da6c2c5f74545b3ea0e5411c524611a26a3
Kenps Rossi <kenpsrossi@gmail.com>
2024-01-10 16:09:10 -0300

    implementação smartphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models // Lógica: Define o namespace para a classe Iphone no contexto do projeto DesafioPOO.Models.
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone // Entrada: Declaração da classe Iphone. Lógica: Define a classe Iphone que herda da classe base Smartphone.
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) // Entrada: Parâmetros do construtor Lógica: Construtor da classe Iphone que repassa os parâmetros para o construtor da classe base Smartphone usando base.
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp) // Entrada: Parâmetro do método (nomeApp).Lógica: Declaração do método InstalarAplicativo, sobrescrito da classe base Smartphone.
+        { 
+            Console.WriteLine($"Instalando {nomeApp} no iPhone..."); // Entrada: Valor da variável nomeApp. Saída: Mensagem formatada na console. Lógica: Imprime uma mensagem indicando que um aplicativo está sendo instalado no iPhone, incluindo o nome do aplicativo fornecido como argumento.
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
-namespace DesafioPOO.Models
+using System; // Lógica: Importa o namespace System para utilizar funcionalidades básicas do C#.
+using DesafioPOO.Models; // Lógica: Importa o namespace DesafioPOO.Models para usar a classe base Smartphone.
+namespace PhonePooProject.Models // Lógica: Define um namespace para a classe Flip no contexto do projeto PhonePooProject.
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Flip : Smartphone // Lógica: Define a classe Flip que herda da classe base Smartphone.
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Flip(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) // Lógica: Construtor da classe Flip que repassa os parâmetros para o construtor da classe base Smartphone usando base.
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp) //Entrada: Parâmetro do método (nomeApp).Lógica: Declaração do método InstalarAplicativo, sobrescrito da classe base Smartphone.
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Flip."); // Entrada: Valor da variável nomeApp. Saída: Mensagem formatada na console. Lógica: Imprime uma mensagem indicando que um aplicativo está sendo instalado no Flip, incluindo o nome do aplicativo fornecido como argumento.
+        }
     }
 }
